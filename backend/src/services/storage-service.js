4c8908d732e904b62b634a4aa80454898aa87e4a
Lola-Nieto <48588470@est.ort.edu.ar>
2025-09-22 12:15:02 -0300

    cambiando para almacenar en buckets

--- backend/src/services/storage-service.js
+++ backend/src/services/storage-service.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import { uploadFile, saveToSupabase } from '../middlewares/upload.js';
 
 export default class StorageService {
-    async uploadFile (file, carpeta){
+    async uploadFile (file, carpeta, userID){
         
         //Validar archivo
         if (!req.file) {
@@ -12,7 +12,25 @@ export default class StorageService {
               .send('No se recibió el archivo. Usa el campo "image".');
           }
 
-        const url = uploadFile(file, carpeta);
+        const nombre = uploadFile(file, carpeta); //qué devuelve?
+        const url = saveToSupabase(); 
+
+
+          if(url){
+            const saved = await db.saveUserProfilePic(userId, fileUrl);
+            if (!saved) {
+              // 4. Si falla la BD → eliminar archivo del bucket
+              await supabase.storage.from(bucket).remove([fileName]);
+              throw new Error('Error guardando en la BD, archivo eliminado del bucket');
+            }
+          }
+        
+  
+        return fileUrl;
+      } catch (err) {
+        console.error('Error en uploadFile:', err.message);
+        throw err;
+      }
       
     }
 };
